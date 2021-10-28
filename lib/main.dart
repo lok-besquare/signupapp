cb207f1747eebe462e2934f0ee3721deffb55e13
Lok <wenglok@besquare.com.my>
2021-10-28 20:53:02 +0800

    Latest Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,162 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SignUpPage());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   final String img_url =
+//       'https://p.kindpng.com/picc/s/20-203381_sign-up-icon-png-login-and-signup-icon.png';
+//   final snackBar = SnackBar(content: Text('You have successfully signed up.'));
+//   final username = TextEditingController();
+//   final email = TextEditingController();
+//   final password = TextEditingController();
+
+//   Widget build(BuildContext context) {}
+// }
+
+class SignUpPage extends StatefulWidget {
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final String img_url =
+      'https://p.kindpng.com/picc/s/20-203381_sign-up-icon-png-login-and-signup-icon.png';
+  final _snackBar = SnackBar(content: Text('You have successfully signed up.'));
+  final _username = TextEditingController();
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+  String _DropdownChoice = 'Male';
+  bool _isButtonDisabled = false;
+  // ignore: non_constant_identifier_names
+  List<DropdownMenuItem<String>> get itemsList {
+    List<DropdownMenuItem<String>> itemsList = [
+      DropdownMenuItem(child: Text("Male"), value: "Male"),
+      DropdownMenuItem(child: Text("Female"), value: "Female"),
+      DropdownMenuItem(child: Text("Other"), value: "Other"),
+      DropdownMenuItem(
+          child: Text("Rather not to say"), value: "Rather not to say"),
+    ];
+    return itemsList;
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  void initState() {
+    _isButtonDisabled = false;
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void checkAllEnter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_username.text.isNotEmpty &&
+          _email.text.isNotEmpty &&
+          _password.text.isNotEmpty) {}
     });
   }
 
+  void buttonStatus() {
+    setState(() {});
+  }
+
+  void directoHome() {}
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Sign-Up Page'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            body: ListView(
+              children: <Widget>[
+                SizedBox(height: 5),
+                Center(
+                  child: new Text(
+                    "Welcome to Sign-Up App!",
+                    style: new TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Image.network(
+                  (img_url),
+                  width: 200,
+                  // fit: BoxFit.cover,
+                ),
+                TextFormField(
+                  // ignore: deprecated_member_use
+                  controller: _username,
+                  cursorColor: Theme.of(context).cursorColor,
+                  maxLength: 20,
+                  style: new TextStyle(),
+                  decoration: InputDecoration(
+                    icon: Icon(IconData(57938, fontFamily: 'MaterialIcons')),
+                    labelText: 'Name',
+                    hintText: 'Enter your name...',
+                    suffixIcon: Icon(
+                      Icons.check_circle,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  // ignore: deprecated_member_use
+                  controller: _email,
+                  cursorColor: Theme.of(context).cursorColor,
+                  maxLength: 20,
+                  // style:TextStyle(color:Colors.amber,
+                  // :20,
+                  // ),
+                  decoration: InputDecoration(
+                    icon: Icon(IconData(57473, fontFamily: 'MaterialIcons')),
+                    labelText: 'Email',
+                    hintText: 'Enter your email...',
+                    suffixIcon: Icon(
+                      Icons.check_circle,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  // ignore: deprecated_member_use
+                  controller: _password,
+                  cursorColor: Theme.of(context).cursorColor,
+                  maxLength: 20,
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    icon: Icon(IconData(58490, fontFamily: 'MaterialIcons')),
+                    labelText: 'Password',
+                    hintText: 'Enter your password...',
+                    suffixIcon: Icon(
+                      Icons.check_circle,
+                    ),
+                  ),
+                ),
+                DropdownButtonFormField(
+                    value: _DropdownChoice,
+                    decoration: InputDecoration(
+                        icon:
+                            Icon(IconData(60881, fontFamily: 'MaterialIcons')),
+                        labelText: 'Please select your gender'),
+                    items: itemsList,
+                    onChanged: (String? storeNewOption) {
+                      setState(() {
+                        _DropdownChoice = storeNewOption!;
+                      });
+                    }),
+                SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  child: Text('Submit'),
+                ),
+              ],
+
+              // child: TextField(
+              //   controller: _controller,
+              // ),
+            )));
   }
 }
